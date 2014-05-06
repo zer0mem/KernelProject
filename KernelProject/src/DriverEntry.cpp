31c50cd22f04d64db67e710e94651d324b064319
Peter Hlavaty <zer0mem@yahoo.com>
2014-05-06 18:32:00 +0800

    kernelproject calls for c++ support, std & boost
    just some demo PoC

--- KernelProject/src/DriverEntry.cpp
+++ KernelProject/src/DriverEntry.cpp
@@ -3,36 +3,88 @@
 * @author created by: Peter Hlavaty
 */
 
-#include "Common.cpp"
+#include "drv_common.h"
+#include <memory>
 
+#include <boost/shared_ptr.hpp>
+#include <boost/intrusive/avltree.hpp>
+
+#include <Common/utils/VadScanner.hpp>
+
+#include <Common/auto/AutoPtr.hpp>
+
+void custom_deler(
+		__in int* mem
+		)
+{
+	DbgPrint("\ncustom_deler to action\n");
+	delete mem;
+}
+
+class xyz
+{
+public:
+	xyz()
+	{
+		DbgPrint("\nctor %p\n", this);
+	}
+	~xyz()
+	{
+		DbgPrint("\ndtor %p\n", this);
+	}
+};
 
 //---------------------------
 //-----   UNINSTALL   -------
 //---------------------------
-void OnUnload(
-	__in DRIVER_OBJECT* DriverObject
-	)
+
+CCppDriver::~CCppDriver()
 {
-	UNREFERENCED_PARAMETER(DriverObject);
-	DbgPrint("\n\n************** unloaded! <<<<<<<<<<<<<<<\n\n");
+	DbgPrint("\n\n************** unloaded! %p <<<<<<<<<<<<<<<\n\n", nullptr);
 } // end OnUnload
 
 //---------------------------
 //------   INSTALL   --------
 //---------------------------
-NTSTATUS DriverEntry(
+
+__checkReturn
+NTSTATUS 
+CCppDriver::Main(
 	__in DRIVER_OBJECT* driverObject, 
-	__in UNICODE_STRING* RegistryPath
+	__in UNICODE_STRING* registryPath
 	)
 {
-	UNREFERENCED_PARAMETER(RegistryPath);
+	UNREFERENCED_PARAMETER(registryPath);
+	//EnviromentDependentVariablesInit();
+		
+	auto superfetch_info = new std::unique_ptr<int, void(*)(int*)>(
+		(int*)malloc(0x100),
+		custom_deler);
+	
+	CAutoMem sup_ft(malloc(0x100));
+	
+	auto superfetch_info_shared = std::shared_ptr< std::unique_ptr<int, void(*)(int*)> >(
+		superfetch_info);
 
-	gDriverObject = driverObject;
-	EnviromentDependentVariablesInit();
+	auto test_shared = std::shared_ptr<xyz>(new xyz());
 
-	_cinit(0);
+	DbgBreakPoint();
+	//NEW USAGE!!
+	CRange<void*> mem(nullptr);
+	if (CVAdScanner::GetMemory((void*)0x77000000, &mem))
+	{
+		DbgPrint("\nfound memory : %p %p\n", mem.Begin(), mem.End());
 
-	driverObject->DriverUnload = OnUnload;
+		std::shared_ptr<CVadNode> sp_vad_node;
+		if (!CVAdScanner::Find(mem, &sp_vad_node))
+			return STATUS_UNSUCCESSFUL;
+
+		for (int i = 0; i < 0x10; i++)
+		{
+			++(*sp_vad_node.get());
+			DbgPrint("\nXX. %p ITERATE : %p %p\n", sp_vad_node.get(), sp_vad_node->Begin(), sp_vad_node->End());
+		}
+	}
 
 	return STATUS_SUCCESS;
 } // end DriverEntry()
