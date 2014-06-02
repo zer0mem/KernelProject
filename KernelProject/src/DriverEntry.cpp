563b8087c317999e3f7f23cd6e341cf954e10234
Peter Hlavaty <zer0mem@yahoo.com>
2014-06-02 21:29:56 +0800

    capstone update, prerelease v2

--- KernelProject/src/DriverEntry.cpp
+++ KernelProject/src/DriverEntry.cpp
@@ -12,14 +12,17 @@
 
 CCppDriver::~CCppDriver()
 {
-	DbgPrint("\n\n************** unloaded! %p <<<<<<<<<<<<<<<\n\n", nullptr);
+	printf("\n\n************** unloaded! %p <<<<<<<<<<<<<<<\n\n", nullptr);
 } // end OnUnload
 
 //---------------------------
 //------   INSTALL   --------
 //---------------------------
 
-void test();
+EXTERN_C
+int main();
+
+#include <Common/3rdparty/Disasm.hpp>
 
 __checkReturn
 NTSTATUS 
@@ -28,6 +31,23 @@ CCppDriver::Main(
 	__in UNICODE_STRING* registryPath
 	)
 {
-	test();
+	UNREFERENCED_PARAMETER(driverObject);
+	UNREFERENCED_PARAMETER(registryPath);
+
+	main();
+
+	CDisasmbler64 dis;
+	auto insn = dis.Disasm(main, 0x30);
+
+	if (!insn.get())
+		return STATUS_SUCCESS;
+
+	printf("\n\nDISASM :\n\n");
+	for (size_t i = 0; i < insn->Count; i++)
+	{
+		printf("-> 0x%p:\t%s\t%s\n", insn->Instructions[i].address, insn->Instructions[i].mnemonic, insn->Instructions[i].op_str);
+		dis.PrintInstDetail(insn->Instructions[i]);
+	}
+
 	return STATUS_SUCCESS;
 } // end DriverEntry()
